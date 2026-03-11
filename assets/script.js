3dcc13929e7313bce7555175c4ab9825c9743e09
Mohamed-Amine <mohamed13.edouiri@gmail.com>
2026-03-11 16:55:06 +0100

    responsive-final

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
-            document.querySelector('.navbar').classList.toggle('nav-open');
-        });
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Fermer le menu quand on clique sur un lien
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
+
+// Fermer le menu quand on clique sur un bouton
+document.querySelectorAll('.nav-links button').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
 
 
